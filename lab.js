71a287a7345c95f45621fb119cf42f0c69bd8f18
NCMuthunayaka <nayanachandupamuthunayaka@gmail.com>
2025-11-14 10:51:05 +0530

    starting with arrays

---
+++ lab.js
@@ -0,0 +1,9 @@
+// creating array of fruits
+const fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+console.log(fruits);
+
+//printing first element and last element of the array
+console.log(fruits[0],fruits[fruits.length-1]);
+
+//adding new element to the array and print it
+fruits.push("pineapple");

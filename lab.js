a58a148c7e80f1e2bee6404e3104ed2060724977
NCMuthunayaka <nayanachandupamuthunayaka@gmail.com>
2025-11-14 11:04:31 +0530

    all answers for the first qestion

--- lab.js
+++ lab.js
@@ -7,3 +7,11 @@ console.log(fruits[0],fruits[fruits.length-1]);
 
 //adding new element to the array and print it
 fruits.push("pineapple");
+console.log("printing last item : ",fruits[fruits.length-1])
+
+//removing second item from the array
+fruits.splice(1,1)
+console.log("new array : ",fruits)
+
+//finding lenth of the array
+console.log(fruits.length)
